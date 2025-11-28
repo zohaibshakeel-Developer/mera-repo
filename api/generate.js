a46fe66f511c4f11909a73920b0df80094ac3bfd
zohaibshakeel-Developer <zohaibshakee@gmail.com>
2025-11-28 22:15:38 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,15 +1,1 @@
-if (mode === "image") {
-  const output = await replicate.run(
-    "stability-ai/stable-diffusion-2-1",
-    { input: { prompt, width: 768, height: 768 } }
-  );
-  return res.json({ type: "image", url: output[0] });
-}
 
-if (mode === "video") {
-  const output = await replicate.run(
-    "anotherjesse/zeroscope-v2-xl",
-    { input: { prompt } }
-  );
-  return res.json({ type: "video", url: output });
-}
